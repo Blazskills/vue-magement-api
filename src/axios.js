6088ead14771e64a9eb9ed20e9aff23ee3de3376
Temitope Ilesanmi <ilesanmiisaac@gmail.com>
2022-08-08 15:53:32 +0100

    update

--- src/axios.js
+++ src/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const  getApi = axios.create({
-    // baseURL: 'https://contactmgt-app.herokuapp.com/',
-     baseURL: 'http://127.0.0.1:4000/',
+    baseURL: 'https://contactmgt-app.herokuapp.com/',
+    //  baseURL: 'http://127.0.0.1:4000/',
     timeout: 10000
 })
 
